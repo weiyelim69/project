a2de62eb683ea6286ab6f4a55c15fbbadc08099e
KaiserKypto <70192434+KaiserKypto@users.noreply.github.com>
2020-08-25 14:45:31 +0800

    Add files via upload

---
+++ Get.Request.py
@@ -0,0 +1,26 @@
+# Use the Request library
+import requests
+# Set the target webpage
+url = 'http://172.18.58.238/photography/'
+r = requests.get(url)
+# This will get the full page
+print(r.text)
+# This will get the status code
+print("Status code:")
+print("\t *", r.status_code)
+# This will just get just the headers
+h = requests.head(url)
+print("Header:")
+print("**********")
+# To print line by line
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
+# This will modify the headers user-agent
+headers = {
+    'User-Agent' : 'Mobile'}
+
+# Test it on an external site
+url2 = 'http://httpbin.org/headers'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
