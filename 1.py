362048edb970187e2e7763f8915c87693d3990a5
weiyelim69 <70191893+weiyelim69@users.noreply.github.com>
2020-08-25 14:28:55 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,19 @@
+import requests
+url = 'http://172.18.58.238/photography/'
+r = requests.get(url)
+print(r.text)
+print("Status code:")
+print("\t *", r.status_code)
+h = requests.head(url)
+print("Header:")
+print("**********")
+
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
+headers = {
+    'User-Agent' : 'Mobile'
+}
+url2 = 'http://httpbin.org/headers'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
